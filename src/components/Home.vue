7e01bfd5291520b22715f6b21422deb52f2d3a42
tinashebvukumbwe <trbvukumbwe1@gmail.com>
2018-02-08 09:35:14 +0200

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <h1>Trade or view your portfolio</h1>
+    <h6>save and load data</h6>
+    <h6>click end day to start a new day</h6>
+    <p>Your Funds: {{ funds | currency }}</p>
+  </div>
+
+</template>
+<script>
+  export default {
+    computed: {
+      funds(){
+        return this.$store.getters.funds;
+      }
+    }
+  }
+</script>
